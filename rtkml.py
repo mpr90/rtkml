a6e5b64f1c98965c03f9a087b00d3f6d60fd46fc
mpr90 <mpr90@yahoo.com>
2021-03-28 14:55:42 -0700

    Add informative messages during operation Add '-d' or '--debug' argument to enable diagnostics Save JSON response data to tripid.json file for debug.

--- rtkml.py
+++ rtkml.py
@@ -17,20 +17,27 @@ except ModuleNotFoundError:
 
 URL = 'https://maps.roadtrippers.com/api/v2/trips/%s'
 
-def export(trip_ids, no_waypoints=False, no_paths=False):
+def export(trip_ids, no_waypoints=False, no_paths=False, debug=False):
     import requests
     import simplekml
     import polyline
     kml = simplekml.Kml()
     for id in trip_ids:
         try:
-            resp = requests.get(URL % id)
+            fullURL = URL % id
+            print("Getting trip details from '%s'" % fullURL)
+            resp = requests.get(fullURL)
             if resp.status_code == 404:
                 print('Error 404\nCould not find trip %s' % id)
                 continue
             if resp.status_code != 200:
                 print('Error %s trying to find trip %s' % (resp.status_code, id))
                 continue
+            if debug:
+                fn = 'response_%s.json' % id
+                print("  Saving JSON response to '%s'" % fn)
+                with open(fn, 'wb') as f:
+                    f.write(resp.content)
             data = resp.json()
             trip = data['trip']
             waypoints = trip['waypoints']
@@ -49,15 +56,18 @@ def export(trip_ids, no_waypoints=False, no_paths=False):
                     if waypoint['start_location'][0] != waypoint['end_location'][0] or waypoint['start_location'][1] != waypoint['end_location'][1]:
                         kml.newpoint(name=waypoint['name'], coords=[(waypoint['end_location'][0], waypoint['end_location'][1])])
         except:
-            print('Error parsing trip %s' % id)
+            e = sys.exc_info()[0]
+            print('Error parsing trip %s : %s' % (id, e))
+
     fn = '-'.join(trip_ids) + '.kml'
     kml.save(fn)
-    print('KML file save to %s' % fn)
+    print('KML file saved to %s' % fn)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('trip_id', nargs='+', help='The ID(s) of the trip(s) you want to export.')
     parser.add_argument('--no-waypoints', action='store_true', help='Omit waypoints from the KML.')
     parser.add_argument('--no-paths', action='store_true', help='Omit paths from the KML.')
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debugging, save JSON response to file.')
     args = parser.parse_args()
-    export(args.trip_id, args.no_waypoints, args.no_paths)
+    export(args.trip_id, args.no_waypoints, args.no_paths, args.debug)
