567b7a69d455efaa11de7020c7acbaba5707823b
mpr90 <mpr90@yahoo.com>
2021-03-29 23:02:06 -0700

    For a multi-day trip, group paths into one path per day. For a single day trip, group all legs into one path. Update readme with additional information on usage and modes of operation.

--- README.md
+++ README.md
@@ -18,3 +18,11 @@ For example, my upcoming road trip is split into 4 segments, but I want to displ
 ### Where do I find my trip IDs?
 
 Go to Roadtrippers, log in, and click on "My Trips". Select the trip you want to export. In the address bar of your browser, you'll see something like this: `https://roadtrippers.com/map?lat=35.00000&lng=-120.00000&z=7&a2=t!12345678`. The last part, after `t!`, is your trip ID. In this case, it's `12345678`.
+
+### Additional Info
+
+Both the data from the waypoints in the RT data and the legs (a.k.a. KML path or GPX Track) will be extracted into the KML. All of the "legs" in the Roadtrippers JSON response data will be combined into a single path. The waypoints can be omitted by using the `--no-waypoints` argument; the paths can be omitted by using the `--no-paths` argument. 
+
+If you have a multi-day trip and include a date on the overnight stops, rtkml will create a single track for each day of the trip. This feature can be disabled by using the `--no-group-days` command line argument, which will then create one KML path for each leg in the JSON response.
+
+To save a copy of the JSON response from RoadTrippers API, use the `--debug` flag. This will be saved in a file named after the trip ID: `<ID>.json`
--- rtkml.py
+++ rtkml.py
@@ -17,7 +17,18 @@ except ModuleNotFoundError:
 
 URL = 'https://maps.roadtrippers.com/api/v2/trips/%s'
 
-def export(trip_ids, no_waypoints=False, no_paths=False, debug=False):
+def MakeTrack(kml, day, coords, distance):
+    import simplekml
+    ls = kml.newlinestring(name="Day %02d"%(day+1))
+    ls.coords = coords
+    ls.extrude = 1
+    ls.altitudemode = simplekml.AltitudeMode.clamptoground
+    ls.style.linestyle.width = 5
+    ls.style.linestyle.color = simplekml.Color.cornflowerblue
+    ls.description = "Distance: %d" % distance
+    return ls
+
+def export(trip_ids, no_waypoints=False, no_paths=False, no_group_days=False, debug=False):
     import requests
     import simplekml
     import polyline
@@ -42,19 +53,58 @@ def export(trip_ids, no_waypoints=False, no_paths=False, debug=False):
             trip = data['trip']
             waypoints = trip['waypoints']
             legs = trip['legs']
-            if not no_paths:
-                for leg in legs:
-                    ls = kml.newlinestring()
-                    ls.coords = [(c[1], c[0], 0.0) for c in polyline.decode(leg['encoded_polyline'])]
-                    ls.extrude = 1
-                    ls.altitudemode = simplekml.AltitudeMode.clamptoground
-                    ls.style.linestyle.width = 5
-                    ls.style.linestyle.color = simplekml.Color.cornflowerblue
+            waypoint_start_date = {}
+            waypoint_name = {}
+
+            # Do waypoints first so we can build the list of waypoint IDs that indicate the end of a day's travelling
             if not no_waypoints:
                 for waypoint in waypoints:
                     kml.newpoint(name=waypoint['name'], coords=[(waypoint['start_location'][0], waypoint['start_location'][1])])
                     if waypoint['start_location'][0] != waypoint['end_location'][0] or waypoint['start_location'][1] != waypoint['end_location'][1]:
                         kml.newpoint(name=waypoint['name'], coords=[(waypoint['end_location'][0], waypoint['end_location'][1])])
+                    # See if the waypoint has a date, if so save it with the ID. The last waypoint with a given date will be
+                    # the one stored in the dict, which allows us to later determine which leg represnts the last leg of a given day.
+                    start_date = waypoint['start_date']
+                    id = waypoint['id']
+                    if start_date:
+                        waypoint_start_date[start_date] = id
+                    waypoint_name[id] = waypoint['name']
+
+            # Do paths (in KML-speak, or "track" in GPX-speak or "leg" in RT-speak)
+            if not no_paths:
+                coords = []
+                day = 0
+                distance = 0
+                waypoint_index = 0
+                commit = False
+                for leg in legs:
+                    end_waypoint_id = leg['end_waypoint_id']
+                    distance = distance + leg['distance'] / 1.609   # Add leg distance (kilometers) to overall day distance (miles)
+                    coords.extend([(c[1], c[0], 0.0) for c in polyline.decode(leg['encoded_polyline'])])
+                    waypoint_index = waypoint_index + 1
+              
+                    if no_group_days:
+                        commit = True
+                    elif end_waypoint_id in waypoint_start_date.values():
+                        name = waypoint_name[end_waypoint_id] if end_waypoint_id in waypoint_name else ''
+                        print("  Found end of day %2d in leg %2d  (%s)" % (day+1, waypoint_index, name))
+                        commit = True
+                    else:
+                        commit = False
+              
+                    if (commit):
+                        MakeTrack(kml, day, coords, distance)
+                        coords = []
+                        distance = 0
+                        day = day + 1
+
+                # Don't forget the last segment commit in case the last waypoint has no date
+                if (not commit):
+                    if not no_group_days:
+                        name = waypoint_name[end_waypoint_id] if end_waypoint_id in waypoint_name else ''
+                        print("  Found end of day %2d in leg %2d  (%s)" % (day+1, waypoint_index, name))
+                    MakeTrack(kml, day, coords, distance)
+
         except:
             e = sys.exc_info()[0]
             print('Error parsing trip %s : %s' % (id, e))
@@ -68,6 +118,7 @@ if __name__ == '__main__':
     parser.add_argument('trip_id', nargs='+', help='The ID(s) of the trip(s) you want to export.')
     parser.add_argument('--no-waypoints', action='store_true', help='Omit waypoints from the KML.')
     parser.add_argument('--no-paths', action='store_true', help='Omit paths from the KML.')
+    parser.add_argument('--no-group-days', action='store_true', help='Do not group individual tracks into days based on dates.')
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debugging, save JSON response to file.')
     args = parser.parse_args()
-    export(args.trip_id, args.no_waypoints, args.no_paths, args.debug)
+    export(args.trip_id, args.no_waypoints, args.no_paths, args.no_group_days, args.debug)
